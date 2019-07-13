a030c9d61e561b9293df5bcef02b4664abd8274c
Gideon-Muriithi <gideongakenge@gmail.com>
2019-07-13 19:17:49 +0300

    Add the names and weekdays arrays

--- js/scripts.js
+++ js/scripts.js
@@ -15,6 +15,8 @@ function theInput(){
     CC = Math.trunc(cc);
     //alert(CC);
    var dayoftheweek =  (((CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7;
-   alert(dayoftheweek);
+   var weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+   var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+   var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
    theInput();
  }
