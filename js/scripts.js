0f8957cabe2e6cfba3881a51faa67cd2eeea8fd6
Gideon-Muriithi <gideongakenge@gmail.com>
2019-07-14 22:45:12 +0300

    Ident the codes

--- js/scripts.js
+++ js/scripts.js
@@ -7,18 +7,14 @@ function theInput(){
    if (document.userInput.gender[i].checked)  {
 
    checkedValue = document.userInput.gender[i].value;
-
-
-     }
+    }
    }
-
-    var sliceyy = yy.slice(0,2);
-
+   var sliceyy = yy.slice(0,2);
     YY = parseInt(sliceyy);
     var forcc = parseInt(yy); //Change year from a string type to an interger.
     var cc =forcc/100; //Create a variable for easy truncation for in a century.
     CC = Math.trunc(cc);
-  
+
    var dayoftheweek =  Math.trunc((((CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7);
 
    var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
@@ -67,5 +63,5 @@ function theInput(){
      alert("Your Akan Name is " + femaleNames[6]);
    }
 
-   theInput();
+ theInput();
  }
